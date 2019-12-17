9320de1b77f4e42d7736ae14dc295016b6e90d92
Fraks51 <semzhuv@yandex.ru>
2019-12-17 14:26:49 +0300

    lab 5 added

--- lab5/upback.sh
+++ lab5/upback.sh
@@ -7,10 +7,10 @@ then
 fi
 
 lastBackup=$(find /home/frak/Backup-* | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}$" | sort -n | tail -n 1)"/"
-
+            
 for file in $(ls $lastBackup);
 do
-    if [ -z "$(echo $file | grep -E "*.[0-9]{4}-[0-9]{2}-[0-9]{2}")" ];
+    if [ ! -z "$(echo $file | grep -E "*.[0-9]{4}-[0-9]{2}-[0-9]{2}")" ];
     then
         cp $lastBackup$file $restore$file
     fi
